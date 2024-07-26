424678c08356473a169c17cf20778daa2cf1c42f
unknown <parmarparth954@gmail.com>
2024-07-26 13:19:05 +0530

    photo phone earth

--- website/src/Pages/App/Components/HeroAnimation.jsx
+++ website/src/Pages/App/Components/HeroAnimation.jsx
@@ -1,5 +1,6 @@
 
 import EarthSVG from "../Home/images/Earth.svg"
+import EarthSVGPhone from "../Home/images/Earthphone.svg"
 import Satelliteimg from "../Home/images/Satellite.png"
 const HeroAnimation = () => {
     return (
@@ -7,6 +8,7 @@ const HeroAnimation = () => {
             <div className="d-flex heroimg_column">
                 <div className="heroimg_container align-self-center ">
                     <img src={EarthSVG} className="Earthsvg" />
+                    <img src={EarthSVGPhone} className="Earthsvg phone-earthimg" />
                 </div>
             </div>
             <div className="d-flex heroimg_column myfix" style={{ position: "absolute", top: "70px" }}>
